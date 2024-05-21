5e44b303056df6ff88f2d1400230021c50dc33ff
Miguel Alanís <miguelalanismx@gmail.com>
2024-05-21 01:11:08 -0600

    Implementacion Scanner

--- src/Principal.java
+++ src/Principal.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Escribe la moneda que deseas convertir");
+        String monedaBase = teclado.nextLine();
+        System.out.println("Escribe la moneda a la que deseas convertir");
+        String mondedaConvertida = teclado.nextLine();
         ConsultaTipoDeCambio consulta = new ConsultaTipoDeCambio();
-        Moneda moneda = consulta.consultaTipoDeCambio("MXN");
-        System.out.println("Por cada " + moneda.base_code() + " hay " + moneda.conversion_rates().get("USD") + " USD ");
+        Moneda moneda = consulta.consultaTipoDeCambio(monedaBase);
+        System.out.println("Por cada " + moneda.base_code() + " hay " + moneda.conversion_rates().get(mondedaConvertida) + " " + mondedaConvertida);
     }
 }
